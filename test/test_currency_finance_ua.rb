a0f2d7c861bed7df7dc994ab259b9b021dc70a0c
ruslanpa <ruslan.pavlutskiy@gmail.com>
2013-10-14 15:07:01 +0300

    - Add stub to test application.

--- test/test_currency_finance_ua.rb
+++ test/test_currency_finance_ua.rb
@@ -3,6 +3,15 @@ require 'currency_finance_ua'
 
 class CurrencyFinanceUATest
 
-  def test_load_data
+  def test_load
+  end
+
+  def test_min_or_max
+  end
+
+  def test_rate_of_exchange
+  end
+
+  def test_find_city_by_id
   end
 end
